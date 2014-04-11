53a266eedd7c2463fa74e1896ef52ba22e454606
FardadJalili <jalili.fardad@gmail.com>
2014-04-11 12:11:15 +0430

    Removed shit, added shit, ...

--- Radiology/admin.py
+++ Radiology/admin.py
@@ -1,11 +1,8 @@
-from Radiology.models import Person, Patient, Doctor, MedicalHistory
+from Radiology.models import Patient, Doctor, MedicalHistory
 from django.contrib import admin
 
 __author__ = 'Navid'
 
-class PersonAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name',)
-    
 class PatientAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name',)
 
@@ -15,7 +12,6 @@ class DoctorAdmin(admin.ModelAdmin):
 class MedicalHistoryAdmin(admin.ModelAdmin):
     list_display = ('muck',)
 
-admin.site.register(Person, PersonAdmin)
 admin.site.register(Patient, PatientAdmin)
 admin.site.register(Doctor, DoctorAdmin)
 admin.site.register(MedicalHistory, MedicalHistoryAdmin)
